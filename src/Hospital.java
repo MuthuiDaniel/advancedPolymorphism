13b84274ad06dfb57dcf7eb4b4f179ee466e00a8
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-03-07 20:18:00 +0300

    added reference a single Doctor and a single Patient

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,2 +1,35 @@
 public class Hospital {
+
+    private Doctor doctor;
+    private Patient patient;
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void displayHospitalDetails() {
+        System.out.println("Hospital Details:");
+        if (doctor != null) {
+            doctor.displayDetails();
+        } else {
+            System.out.println("Doctor not assigned yet.");
+        }
+        if (patient != null) {
+            patient.displayDetails();
+        } else {
+            System.out.println("Patient not admitted yet.");
+        }
+    }
 }
