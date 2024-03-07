d8bfe3c22d416d4b6928babd21f6b1ab86ca3f39
MuthuiDaniel <wambua.daniel@strathmore.edu>
2024-03-07 20:16:38 +0300

    added ne unique private attribute and appropriate getter and setter methods

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,24 @@
-public class Patient {
+public class Patient extends Person {
+
+    private String condition;
+
+    public Patient(String name, int age, String condition) {
+        super(name, age);
+        this.condition = condition;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Condition: " + condition);
+    }
 }
+
